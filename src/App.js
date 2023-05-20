fc61e1922e7a8e9148f1ed4aab0576ab99c2bcaa
danis30 <valeprada1103@gmail.com>
2023-05-19 19:14:38 -0500

    ojala se agreme

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import imagen from "./conejo.jpg";
+
+
+function App() {
+  let nombre=[{name:"Jab",edad:19},{name:"Pedrin",edad:3}];
+  let caja =<div>nombre</div>;
+  return (
+    <div className="fondo">
+      <h1>{nombre.edad}</h1>
+     <img src={imagen} className="img"/>
+     <div>{caja}</div>
+     <input value={nombre[1].name}/>
+      <br/>
+    </div>
+  );
+}
+
+export default App;
